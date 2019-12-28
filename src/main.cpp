6dc92562f9b2b0760f8f4f227d2b547b160f0402
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-12-28 13:01:14 -0500

    Removed old print code for the ast

--- src/main.cpp
+++ src/main.cpp
@@ -35,74 +35,14 @@ void print_tokens(vector<Token> tokens)
   }
 }
 
-void print_node(Node node, vector<Position> &end_positions)
-{
-  if (node.type != -1)
-  {
-    if (node.then.nodes.size() > 0)
-    {
-      for (int i = 0; i < node.then.nodes.size(); i++)
-      {
-        print_node(node.then.nodes[i], end_positions);
-      }
-    }
-    else
-    {
-      // ! Store parent in node, so that we can check if it's inside the while loop or if statement, to print -- END THEN --
-      // ? If node.parent.end.line_number == end_positions[i].line_number
-      for (int i = 0; i < end_positions.size(); i++)
-      {
-        cout << end_positions[i].line_number << ' ' << end_positions[i].line_position << endl;
-        cout << node.parent->then.end.line_number << ' ' << node.parent->then.end.line_position << endl;
-        // cout << end_positions[i
-        // cout << end_positions[i].line_number << ' ' << node.then.end.line_number << endl;
-        // Node * parent_node->node.parent;
-        // if (end_positions[i].line_number == node.parent.end.line_number && end_positions[i].line_position == node.parent.end.line_position)
-        // {
-        // cout << "------ END THEN ------" << endl;
-        // }
-        // else if(end_positions[i].line_number == node. && end_positions[i].line_position == node.then.end.line_position)
-      }
-      if (node.type == Node_Types::_if)
-      {
-        cout << "------ IF ------" << endl;
-        cout << endl
-             << "------ CONDITION ------" << endl;
-        cout << node.condition.left.value << ' ' << node.condition.op.value << ' ' << node.condition.right.value << endl;
-        cout << "------ THEN ------" << endl;
-        end_positions.push_back(node.then.end);
-      }
-      if (node.type == Node_Types::print)
-      {
-        cout << "PRINT - " << node.print_value << endl;
-      }
-      // cout << " => " << node.type << endl;
-    }
-  }
-}
-
 void print_ast(Tree ast)
 {
-  // cout << ast.nodes[0] << endl;
-
   for (int i = 0; i < ast.nodes.size(); i++)
   {
     cout << "------ NODE ------" << endl;
     cout << ast.nodes[i] << endl;
     cout << "------ END NODE ------" << endl;
   }
-  //   vector<Position> end_positions;
-  //   cout << "------ START AST ------" << endl;
-  //   cout << endl;
-
-  //   for (int i = 0; i < ast.nodes.size(); i++)
-  //   {
-  //     Node node = ast.nodes[i];
-  //     print_node(node, end_positions);
-  //   }
-
-  //   cout << endl;
-  //   cout << "------ END AST ------" << endl;
 }
 
 int main(int argc, char **argv)
