05586b72569c66dc0c9b015ba724aecf58173d15
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-03-31 19:11:54 -0400

    Fix function parameter parsing

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,17 +3,18 @@
 std::vector<std::string> Functions::get_fn_parameters(std::vector<Lexer::Token> tokens, int i)
 {
   std::vector<std::string> params;
-
-  for(int x = i; x < tokens.size(); x++)
+  int counter = 0;
+  for (int x = i; x < tokens.size(); x++)
   {
-    if(tokens[x].value == ")" && tokens[x].type == Lexer::Token_Types::sep)
+    if (tokens[x].value == ")" && tokens[x].type == Lexer::Token_Types::sep)
     {
       return params;
     }
-    if(x % 2 != 0)
+    if (counter % 2 == 0)
     {
       params.push_back(tokens[x].value);
     }
+    counter++;
   }
   return params;
 }
@@ -21,10 +22,11 @@ std::vector<std::string> Functions::get_fn_parameters(std::vector<Lexer::Token> 
 int Functions::get_fn_return_type(std::vector<Lexer::Token> tokens, int i)
 {
   std::string type = tokens[i].value;
-  if(type == "void")
+  if (type == "void")
   {
     return Variables::Variable_Types::_void;
-  } else if (type == "int")
+  }
+  else if (type == "int")
   {
     return Variables::Variable_Types::integer;
   }
@@ -36,16 +38,17 @@ Parser::Then Functions::get_fn_then(std::vector<Lexer::Token> tokens, int i)
   Parser::Then then;
   int open_curly_brackets = 1;
   int closed_curly_brackets = 0;
-  for(int x = i; x < tokens.size(); x++)
+  for (int x = i; x < tokens.size(); x++)
   {
-    if(open_curly_brackets == closed_curly_brackets)
+    if (open_curly_brackets == closed_curly_brackets)
     {
       break;
     }
-    if(tokens[x].type == Lexer::Token_Types::sep && tokens[x].value == "{")
+    if (tokens[x].type == Lexer::Token_Types::sep && tokens[x].value == "{")
     {
       open_curly_brackets++;
-    } else if(tokens[x].type == Lexer::Token_Types::sep && tokens[x].value == "}")
+    }
+    else if (tokens[x].type == Lexer::Token_Types::sep && tokens[x].value == "}")
     {
       closed_curly_brackets++;
     }
